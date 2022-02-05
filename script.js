639ae3cf95a9d17fc2a1c78d1499f8691df1129b
KlerKpn <andrey.invorkel@mail.ru>
2022-02-05 15:17:10 +0300

    [add] stream

---
+++ script.js
@@ -0,0 +1,86 @@
+window.onload = () => redirect('home')
+
+const container = document.querySelector('#app') // заносим в переменную div
+const navList = document.querySelectorAll('[data-link]') // берем все элементы по атрибуту
+
+navList.forEach((el) => {
+  // добавляем на ссылки с атрибутом data-link "навигацию"
+  el.addEventListener('click', () => {
+    const attr = el.getAttribute('data-link')
+    redirect(attr)
+  })
+})
+
+const components = {
+  // создаем шаблонные компоненты
+  create(element, value, attrs) {
+    const el = document.createElement(element)
+    el.innerHTML = value
+    attrs &&
+      Object.entries(attrs).forEach(([key, value]) =>
+        el.setAttribute(key, value),
+      )
+    return el
+  },
+  input: {
+    get email() {
+      // readonly
+      const el = document.createElement('input')
+      el.setAttribute('type', 'email')
+      el.setAttribute('placeholder', 'email')
+      return el
+    },
+    get pass() {
+      const el = document.createElement('input')
+      el.setAttribute('type', 'password')
+      el.setAttribute('placeholder', 'password')
+      return el
+    },
+  },
+  button(value, type) {
+    const el = document.createElement('button')
+    el.innerText = value
+    el.setAttribute('type', type)
+    return el
+  },
+}
+
+// console.log(components.input.create({ type: 'text', value: 'kek' })) // example
+
+const values = {
+  home: () => {
+    const main = components.create('h1', 'Home')
+    return main
+  },
+  about: () => {
+    const main = components.create('h1', 'About')
+    return main
+  },
+  form: () => {
+    const main = components.create('form', 'Форма', { class: 'form' })
+    const list = [
+      components.input.email,
+      components.input.pass,
+      components.create('button', 'Submit', { type: 'submit' }),
+    ]
+
+    list.forEach((element) => main.appendChild(element)) // цикл для примера
+    return main
+  },
+  live: () => {
+    const play = '?autoplay=1'
+    const main = components.create('iframe', '', {
+      // src: 'https://www.youtube.com/embed/PY8f1Z3nARo', live stream
+      src: `https://www.youtube.com/embed/Klt1EBFjt1g${play}`, // YT video require "embed"
+      autoplay: true,
+      frameborder: '0',
+      allowfullscreen: true,
+    })
+    return main
+  },
+}
+
+const redirect = (link) => {
+  container.innerHTML = '' // очищаем предыдущее содержимое
+  container.appendChild(values[link]()) // присваиваем новое отслеживая ссылку и вызываем функцию
+}
